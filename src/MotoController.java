dc93921078388682d86cddc9f3c60c069e9f88f1
alyssonPereiraDS <jose.apereira@mercadolivre.com>
2022-11-08 18:47:02 -0300

    Adicionado classe MotoController.

---
+++ src/MotoController.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class MotoController implements MotoRepository{
+
+    private ArrayList<Moto> listaContas = new ArrayList<Moto>();
+    @Override
+    public void cadastrarMotocicleta() {
+
+    }
+
+    @Override
+    public void editarMotocicleta() {
+
+    }
+
+    @Override
+    public void listarTodas() {
+
+    }
+
+    @Override
+    public void removerMotocicleta() {
+
+    }
+}
